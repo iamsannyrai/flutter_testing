696fc0c7ad497f0106adfb47de2617c0b5abf802
Sanny Rai <sannyrai@sannys-MacBook-Pro.local>
2022-07-10 10:47:44 +0545

    grouping of unit tests

--- lib/maths_util.dart
+++ lib/maths_util.dart
@@ -1,1 +1,7 @@
 int add(int a, int b) => a + b;
+
+int sub(int a, int b) => a - b;
+
+int mul(int a, int b) => a * b;
+
+double div(int a, int b) => a / b;
--- test/maths_util_test.dart
+++ test/maths_util_test.dart
@@ -2,15 +2,53 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:testing/maths_util.dart';
 
 void main() {
-  test('addition of two number', () {
-    // arrange
-    const int a = 10;
-    const int b = 10;
+  group('Math Util', () {
+    test('addition of two number', () {
+      // arrange
+      const int a = 10;
+      const int b = 10;
 
-    // act
-    final sum = add(a, b);
+      // act
+      final result = add(a, b);
 
-    //assert
-    expect(sum, 20);
+      //assert
+      expect(result, 20);
+    });
+
+    test('subtraction of two number', () {
+      // arrange
+      const int a = 10;
+      const int b = 10;
+
+      // act
+      final result = sub(a, b);
+
+      //assert
+      expect(result, 0);
+    });
+
+    test('multiplication of two number', () {
+      // arrange
+      const int a = 10;
+      const int b = 10;
+
+      // act
+      final result = mul(a, b);
+
+      //assert
+      expect(result, 100);
+    });
+
+    test('division of two number', () {
+      // arrange
+      const int a = 10;
+      const int b = 10;
+
+      // act
+      final result = div(a, b);
+
+      //assert
+      expect(result, 1);
+    });
   });
 }
