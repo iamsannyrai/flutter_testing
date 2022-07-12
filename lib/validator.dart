772558501d9edabb83dc3b677ccd7ef3d86b4a2d
Sanny Rai <sannyrai@sannys-MacBook-Pro.local>
2022-07-12 09:13:55 +0545

    unit test for email and password validation

---
+++ lib/validator.dart
@@ -0,0 +1,22 @@
+abstract class Validator {
+  static const String _emailPattern = r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$';
+
+  static String vaildateEmail(String email) {
+    if (email.isEmpty) {
+      return "Required Field";
+    } else {
+      RegExp regExp = RegExp(_emailPattern);
+      if (regExp.hasMatch(email)) {
+        return "";
+      }
+      return "Invalid Email";
+    }
+  }
+
+  static String validatePassword(String password) {
+    if (password.isEmpty) {
+      return "Required Field";
+    }
+    return "";
+  }
+}
---
+++ test/validator_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:testing/validator.dart';
+
+void main() {
+  group('Email Validator', () {
+    test('checks if email is empty', () {
+      const String email = "";
+      String result = Validator.vaildateEmail(email);
+      expect(result, "Required Field");
+    });
+
+    test('checks if email is invalid', () {
+      const String email = "yoemail";
+      String result = Validator.vaildateEmail(email);
+      expect(result, "Invalid Email");
+    });
+
+    test('checks if email is valid', () {
+      const String email = "test@test.com";
+      String result = Validator.vaildateEmail(email);
+      expect(result, "");
+    });
+  });
+
+  group('Password Validator', () {
+    test('checks if password is empty', () {
+      const String password = "";
+      String result = Validator.validatePassword(password);
+      expect(result, "Required Field");
+    });
+
+    test('checks if password is valid', () {
+      const String password = "password";
+      String result = Validator.validatePassword(password);
+      expect(result, "");
+    });
+  });
+}
