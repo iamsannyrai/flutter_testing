5b399d2a78410e947908c271e623510998fc92e4
Sanny Rai <sannyrai@sannys-MacBook-Pro.local>
2022-07-13 13:29:18 +0545

    group multiple tests

--- unit_testing/test/counter_test.dart
+++ unit_testing/test/counter_test.dart
@@ -2,20 +2,25 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:unit_testing/counter.dart';
 
 void main() {
-  test('initial value is 0', () {
-    final counter = Counter();
-    expect(counter.value, 0);
-  });
+  group('Counter', () {
+    late Counter counter;
 
-  test('value is incremented on invoking increment function', () {
-    final counter = Counter();
-    counter.increment();
-    expect(counter.value, 1);
-  });
+    setUp(() {
+      counter = Counter();
+    });
 
-  test('value is decremented on invoking decrement function', () {
-    final counter = Counter();
-    counter.decrement();
-    expect(counter.value, 1);
+    test('initial value is 0', () {
+      expect(counter.value, 0);
+    });
+
+    test('value is incremented on invoking increment function', () {
+      counter.increment();
+      expect(counter.value, 1);
+    });
+
+    test('value is decremented on invoking decrement function', () {
+      counter.decrement();
+      expect(counter.value, -1);
+    });
   });
 }
